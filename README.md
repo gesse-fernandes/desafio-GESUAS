9adcc03ca5cd61c0e6111a31fc961add20db52bf
gesse-fernandes <gesse-fm@hotmail.com>
2023-02-12 17:00:09 -0300

    configurando o arquivo README.md

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# aqui é um desafio
+# Deafio para vaga Desenvolvedor(a) Full Stack Pleno :
+
+ este desafio foi criado para avaliar meus conhecimentos técnicos a partir de uma seleção para uma vaga full-stack
+
+
+# Solicitado
+
+  1. Crie uma aplicação contendo um formulário para cadastrar cidadãos. 
+  2. O formulário deve conter um único campo para informar o nome do cidadão. 
+  3. Ao ser cadastrado, um número NIS deve ser gerado automaticamente, atribuído a esta pessoa e então exibido na tela junto de uma mensagem de sucesso. 
+  4. Deve ser possível também pesquisar os registros já existentes através do número NIS. 
+  5. Caso o NIS informado já esteja cadastrado, a aplicação deve exibir o nome do cidadão e seu número NIS. 
+  6. Caso não esteja, deve exibir: “Cidadão não encontrado”. 
+  7. Lembre-se de criar um README contendo as instruções necessárias para executarmos a aplicação
+
+# regras
+ 1. O backend da aplicação deve ser escrito em PHP; 
+ 2. O código deve ser escrito com o paradigma da Programação Orientada a Objetos; 
+ 3. Não é permitido usar nenhum framework para criação de aplicações inteiras como Symfony ou Laravel. Mas você pode usar "frameworks" para tarefas específicas, como o PHPUnit para testes por exemplo.
+ 4. Você pode usar qualquer outra biblioteca de terceiros que desejar. 
+
+ # o oque será avaliado
+
+ 1. O funcionamento correto da aplicação de acordo com os requisitos do desafio;
+
+ 2. A arquitetura da aplicação; 
+
+ 3. A qualidade, clareza e organização do código entregue. Iremos ler cada linha de código, então capriche!
+
+ 4. A utilização de boas práticas de desenvolvimento.
--- app/DataBase/SistemaCidadao.sql
+++ app/DataBase/SistemaCidadao.sql
@@ -1,8 +1,62 @@
+CREATE DATABASE  IF NOT EXISTS `sistemacidadao` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `sistemacidadao`;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Fev-2023 às 20:58
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
 
-CREATE database sistemacidadao;
-use sistemacidadao;
-CREATE TABLE citizens (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  nis VARCHAR(15) NOT NULL UNIQUE
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sistemacidadao`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `citizens`
+--
+
+CREATE TABLE `citizens` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `nis` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `citizens`
+--
+ALTER TABLE `citizens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nis` (`nis`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `citizens`
+--
+ALTER TABLE `citizens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
